2990dc53094b6859a1287a3b3f4f5dab20f54c9f
Cristian Mircea Messel <mess110@gmail.com>
2014-02-05 00:18:35 +0200

    add radio history

--- radio.rb
+++ radio.rb
@@ -8,16 +8,17 @@ end
 
 helpers do
   def playing?
-    # need the output, this one is blocking
     !`ps -e | grep mpg321`.empty?
   end
 end
 
 get '/' do
+  @stations = [['Radio Cluj', 'http://89.238.227.6:8384']]
   haml :index
 end
 
 post '/play' do
+  run_cmd "killall -9 mpg321"
   run_cmd "mpg321 #{params[:url]}"
   redirect '/'
 end
@@ -38,11 +39,16 @@ __END__
 
 @@ index
 - if playing?
-  %form{:action => "/stop", :method => "POST"}
-    %input{:type => "submit", :value => "stop"}
-%form{:action => "/play", :method => "POST"}
-  %input{:type => "text", :name => "url"}
-  %input{:type => "submit", :value => "play"}
+  %form{:action => '/stop', :method => 'POST'}
+    %input{:type => 'submit', :value => 'stop'}
+%form{:action => '/play', :method => 'POST'}
+  %input{:type => 'text', :name => 'url'}
+  %input{:type => 'submit', :value => 'play'}
 %ul
-  %li
-    http://89.238.227.6:8384 - Radio Cluj
+  - @stations.each do |station|
+    %li
+      %form{:action => '/play', :method => 'POST'}
+        = station[0]
+        %input{:type => 'hidden', :name => 'url', :value => station[1]}
+        %input{:type => 'submit', :value => 'play'}
+        = station[1]
