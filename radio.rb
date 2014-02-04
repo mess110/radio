28d5bb8a96aa643ba4e4f9c787d94bb44f52dd25
Cristian Mircea Messel <mess110@gmail.com>
2014-02-04 23:30:44 +0200

    add rest interface for mpg321

--- radio.rb
+++ radio.rb
@@ -1,6 +1,48 @@
 require 'sinatra'
+require 'sinatra/reloader' if development?
 require 'haml'
 
-get '/' do
-  'radio'
+def run_cmd cmd
+  Process.detach(fork{ exec "#{cmd} &"})
 end
+
+helpers do
+  def playing?
+    # need the output, this one is blocking
+    !`ps -e | grep mpg321`.empty?
+  end
+end
+
+get '/' do
+  haml :index
+end
+
+post '/play' do
+  run_cmd "mpg321 #{params[:url]}"
+  redirect '/'
+end
+
+post '/stop' do
+  run_cmd "killall -9 mpg321"
+  redirect '/'
+end
+
+__END__
+
+@@ layout
+%html
+  %head
+    %title radio
+  %body
+    = yield
+
+@@ index
+- if playing?
+  %form{:action => "/stop", :method => "POST"}
+    %input{:type => "submit", :value => "stop"}
+%form{:action => "/play", :method => "POST"}
+  %input{:type => "text", :name => "url"}
+  %input{:type => "submit", :value => "play"}
+%ul
+  %li
+    http://89.238.227.6:8384 - Radio Cluj
